a56c21d83dc6bdc67352935fbe08edb57d206550
Dashken2009 <dashken01012009@gmail.com>
2022-12-03 19:54:25 +0200

    Homework task1,2,3,4,5

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 print('import antigravity')
 print('import __hello__')
-
 print('from __future__ import braces')
+
+print('''
+*\t\t  *\t\t *  *\t*\t\t*\t\t *       
+* *\t\t * *\t*\t\t *\t\t*\t\t* *
+*  *\t * *  *\t\t\t *\t\t*\t\t* *
+*   *\t*\t* *\t\t\t *\t\t*\t   *   *
+*   * * * * * * * *\t\t* * * * *\t * * * * *
+*   * *\t\t  *\t\t*\t*\t\t*\t *\t\t *
+*   * *\t\t  *\t\t*\t*\t\t*\t *\t\t *
+*  *  *\t\t  * \t* \t*\t\t*\t *\t\t *
+* *\t  *\t\t  * * *\t\t*\t\t*\t *\t\t *
+''')
+
+print('''
+\\a      Bell (alert) 
+\\b      Backspace
+\\n      New line
+\\t      Horizontal tab
+\\\      Backslash \\
+\\"      Double quotation mark "
+\\'      Single quotation mark '
+''')
